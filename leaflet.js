1e5a661710fbe89aa901360ee457a43ca3e8cef7
weihricha <100720381+weihricha@users.noreply.github.com>
2024-02-02 18:19:21 -0500

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,20 +1,28 @@
-var map = L.map('map').setView([51.505, -0.09], 13)
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
+ var map = L.map('mapid').setView([51.505, -0.09], 3);
+        
+        fetch('map.geojson')
+ .then(response => response.json())
+ .then(data => {
+   L.geoJSON(data).addTo(map);
+ });
 
-fetch('map.geojson')
-.then(response => response.json())
-.then(data => {
-  L.geoJSON(data).addTo(map);
-});
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+            function onMapClick(e) {
+            alert("You clicked the map at " + e.latlng);
+            }
+
+            map.on('click', onMapClick);
+            var popup = L.popup();
+
+            function onMapClick(e) {
+            popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+            }
+
+        map.on('click', onMapClick);
