d273b8a339762408ae144b0941f817e96df584d7
weihricha <100720381+weihricha@users.noreply.github.com>
2024-02-02 18:06:34 -0500

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,3 +1,18 @@
+var map = L.map('map').setView([51.505, -0.09], 13)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+
 fetch('map.geojson')
 .then(response => response.json())
 .then(data => {
